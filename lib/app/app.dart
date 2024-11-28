e635fef9129fc0055be1d977acb07346bbfc4091
Alejandro Nuñez <nunezg.john@gmail.com>
2024-11-28 09:23:14 -0500

    add: firebase dependencies and localization config

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import '../core/router/router.dart';
+import '../gen/l10n.dart';
 
 class MomentApp extends StatelessWidget {
   const MomentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final router = CustomRouter.router;
+
+    return MaterialApp.router(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      localizationsDelegates: const [
+        IntlTranslations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      // TODO: Implement locale change
+      //locale: locale,
+      supportedLocales: IntlTranslations.delegate.supportedLocales,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
     );
   }
 }
