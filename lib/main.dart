ba3221a5efe4070dbe2a71128516cd66a2e99519
Alejandro Nuñez <nunezg.john@gmail.com>
2024-11-28 08:20:34 -0500

    add: firebase config - init and minimum deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:moment/core/config/config.dart';
 
 import 'app/app.dart';
 
-void main() => runApp(const MomentApp());
+Future<void> main() async {
+  initConfig();
+  //await injection.initInjections();
+  runApp(
+    const MomentApp(),
+  );
+}
